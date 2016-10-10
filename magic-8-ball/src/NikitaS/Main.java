50aaf3515003d3bc55a3692aabbbc6b32accb71b
NikShuvalov <nshuvalov89@gmail.com>
2016-10-10 14:17:27 -0400

    First bonus feature: Added a delay to getting an answer.

--- magic-8-ball/src/NikitaS/Main.java
+++ magic-8-ball/src/NikitaS/Main.java
@@ -8,19 +8,59 @@ public class Main {
     static Random randomGen;
     static String initialPrompt= "Would you like the magic 8 ball to read your mind and answer your most complicated life questions (y/n)";
     static String repeatPrompt = "Would you like to channel the wisdom of the magic 8 ball again? (y/n)";
-    static String[] replies = {"404: divination not found.", "Yes", "No, mayonaisse is not an instrument", "Answer unclear, but future looks bright", "Your answer is in another castle", "Time will tell", "What are you doing consulting an eight ball for your life questions?", "I can't tell you that now, \nbut I can tell you I saved 15% by switching to GEICO","Indoobitably"};
+    static String[] replies = {"404: divination not found.", "Yes", "No, mayonnaise is not an instrument", "Answer unclear, but future looks bright", "Your answer is in another castle", "Time will tell", "What are you doing consulting an eight ball for guidance?", "I can't tell you that now, \nbut I can tell you I saved 15% by switching to GEICO","Indoobitably"};
+    static String[] delayFillers ={"They are telling me...\n", "The spirits...","I'm beginning to see something" ,"...", "Okay, I have your question."};
+
+    static String eightBallArt1 = "\n"+
+            "   #######\n" +
+            "  #########\n" +
+            " ###     ###\n" +
+            " ##  ###  ##\n" +
+            " ##  # #  ##\n" +
+            " ##  ###  ##\n" +
+            " ##  # #  ##\n" +
+            " ##  ###  ##\n" +
+            " ###     ###\n" +
+            "  #########\n" +
+            "   ####### \n";
+
+    static String eightBallArt2 = "\n"+
+            "    #######\n" +
+            "   #########\n" +
+            "  ###     ###\n" +
+            "  ##  ###  ##\n" +
+            "  ##  # #  ##\n" +
+            "  ##  ###  ##\n" +
+            "  ##  # #  ##\n" +
+            "  ##  ###  ##\n" +
+            "  ###     ###\n" +
+            "   #########\n" +
+            "    ####### \n";
+
+    static String eightBallArt3 = "\n"+
+            "     #######\n" +
+            "    #########\n" +
+            "   ###     ###\n" +
+            "   ##  ###  ##\n" +
+            "   ##  # #  ##\n" +
+            "   ##  ###  ##\n" +
+            "   ##  # #  ##\n" +
+            "   ##  ###  ##\n" +
+            "   ###     ###\n" +
+            "    #########\n" +
+            "     ####### \n";
 
     static boolean initial= true;
     //ToDo: Add ASCII art
-    //ToDo: Add either sleep() or countDownTimer()
     //ToDo: Animate 8-ball shake?
     //ToDo: Increase interactivity?
     //Idea: Collect royalties from Geico and do things, like buy an item to go to next castle to get the answer you need.
+    //Change replies to include: It's dangerous to go alone, here take this. Eventually turns into a text-based rpg.
+    //I'm going to make this into a text based rpg.
 
     public static void main(String[] args) {
         randomGen = new Random();
         askUser();
-
     }
 
     public static void askUser(){
@@ -41,6 +81,7 @@ public class Main {
         }
     }
 
+
     public static void shakeTheBall() {
         initial=false;
         int indexNum = getRandomNumber();
@@ -51,7 +92,26 @@ public class Main {
         askUser();
 
     }
+    public static void delay(int time){
+        try {
+            Thread.sleep(1250);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (time>=1){
+            System.out.println(delayFillers[time-1]);
+            delay(time-1);
+        }
+    }
+
     public static void giveReply(int indexNum){
+        System.out.println("Think of your question and I will telepathically read it.");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        delay(5);
         switch(indexNum){
             case 0:
                 System.out.println(replies[indexNum]+"\n");
@@ -84,10 +144,7 @@ public class Main {
 
     }
 
-    public static int getRandomNumber(){
-        return randomGen.nextInt(9);
-    }
-
+    public static int getRandomNumber(){return randomGen.nextInt(9);}
 
     public static String getReply(String prompt){
         System.out.println(prompt + " :");
